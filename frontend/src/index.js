dd66e61427227e4d22837a3736d0c93b0bdb0b37
Sayan Majumder <95490367+sayan404@users.noreply.github.com>
2023-08-29 01:24:43 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,33 +24,3 @@ root.render(
     </AlertProvider>
   </Provider>
 );
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux'
-import store from './Store'
-import { Provider as AlertProvider, transitions, positions } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-const options = {
-timeout : 3000 ,
-position : positions.BOTTOM_CENTER,
-transition : transitions.SCALE
-}
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...options} >
-      <Router>
-        <App />
-      </Router>
-    </AlertProvider>
-  </Provider>
-);
-
-*/
