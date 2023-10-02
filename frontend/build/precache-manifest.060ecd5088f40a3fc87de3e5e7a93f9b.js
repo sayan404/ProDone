130264be0c66af419d48348db0a182e2c2ba65d5
sayan404 <sayanmajumder0002@gmail.com>
2023-10-02 15:43:33 +0530

    updated the build file
    updated the build file

---
+++ frontend/build/precache-manifest.060ecd5088f40a3fc87de3e5e7a93f9b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8039a640d28939b814e5",
+    "url": "/static/css/main.189c726d.chunk.css"
+  },
+  {
+    "revision": "8039a640d28939b814e5",
+    "url": "/static/js/main.8039a640.chunk.js"
+  },
+  {
+    "revision": "5428e0734524485a2467",
+    "url": "/static/css/1.d0630c79.chunk.css"
+  },
+  {
+    "revision": "5428e0734524485a2467",
+    "url": "/static/js/1.5428e073.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3f77d3e801ac49ba3b8832ce59ab1e8",
+    "url": "/static/media/productError.f3f77d3e.png"
+  },
+  {
+    "revision": "abe3db2c01d5641ff796272008bfed86",
+    "url": "/static/media/ProDoneLogoTSP.abe3db2c.png"
+  },
+  {
+    "revision": "9fc3517c2172807493db3c0a06c96f01",
+    "url": "/static/media/newsletter.9fc3517c.svg"
+  },
+  {
+    "revision": "fde27e29db19523979f1a94644c6d2e3",
+    "url": "/index.html"
+  }
+];
